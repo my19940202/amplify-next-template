1206826314c1b8e1d4bbaf0e76043a34ac084dd0
xishengbo <xishengbo@gmail.com>
2026-06-07 22:40:57 +0800

    add google code

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,6 +21,24 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        {/* Google Analytics */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-C3DLQX1GQR"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-C3DLQX1GQR');
+            `,
+          }}
+        />
+      </head>
+
       <body className={inter.className}>{children}</body>
     </html>
   );
